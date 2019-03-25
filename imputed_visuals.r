a99ce5e1e3f55cf14836acf8adfb8bb5108db92e
Michael Esposito <esposm2@uw.edu>
2019-03-25 12:29:14 -0400

    updated model years

--- imputed_visuals.r
+++ imputed_visuals.r
@@ -1,8 +1,10 @@
 #####################################
 # deaths ~ f(age; race; gender)
 # imputed model visuals, .R
-# last edit: 3/6 (M.E)
+# last edit: 3/25 (M.E)
 #####################################
+
+sim = dfs[[1]] %>% mutate(age_group = fct_relevel(age, '5-9', after = 2))
 
 # ... plot it out
 # ... year effects
@@ -17,79 +19,54 @@ dfs[[1]] %>%
 	add_fitted_draws(fit) %>%
 	median_qi(.width = .75) %>%
 	ggplot(., 
-		aes(x = year, 
-			y = .value, 
-			ymin = .lower, 
-			ymax = .upper,
-		)
+		aes(x = year, y = .value, ymin = .lower, ymax = .upper)
 	) +
-	geom_vline(xintercept = 2010, alpha = .25, color = cols[12], size = 4) +
 	geom_errorbar(width = 0, color = cols[8]) +
-	geom_point(fill = cols[13], size = 2.5, pch = 21)
+	geom_point(fill = cols[13], size = 2.5, pch = 21) + 
+	coord_flip()
 
-# ... age x sex gender effects
-demo_scen = dfs[[1]] %>%
+# ... get posterior sims
+demo_scen = sim %>%
 	data_grid(
-		year = 0, 
+		year = 0,
 		age_group,
 		race,
 		sex,
 		pop = 100000
-	) 
-	
-demo_scen %>%
+	)
+
+# ... fitted 
+post_fit = demo_scen %>%
+	mutate(pop = 100000) %>%
 	add_fitted_draws(fit, allow_new_levels = TRUE) %>%
-	median_qi(.width = .5) %>%
-	ggplot(., aes(
-		x = age_group, y = .value, 
-		color = race, fill = race, group = race
-		)
+	median_qi(.width = .9)
+
+# ... predicted
+post_pred = demo_scen %>%
+	mutate(pop = 100000*1000) %>%
+	add_predicted_draws(fit, allow_new_levels = TRUE) %>%
+	median_qi(.width = c(.5,.9))
+
+# ... plot of fitted values
+post_fit %>%
+	ggplot(., 
+		aes(x = age_group, y = .value, color = race, fill = race, group = race)
 	) +
-	#geom_ribbon(alpha = .25, color = 'grey100') +
 	geom_line(size = 1) +
 	facet_wrap(~sex, scale = 'free_y') +
-    c_pal +
-    f_pal + 
-    theme(
+	c_pal +
+	f_pal +
+	theme(
     	axis.text.x = element_text(angle = 90, vjust = 0.5),
     	legend.title = element_blank()
     ) +
-    ylab('rate of being killed by police, per 100k pop') +
+    ylab('risk of being killed by police, per 100k pop.') +
     xlab('age')
 
-# ... predicted values
-# ... men
-demo_scen %>%
-	mutate(pop = 100000*10) %>%
+# ... plot of predicted values
+# ... men 
+post_pred %>%
 	filter(sex == 'Male') %>%
-	add_predicted_draws(fit, allow_new_levels = TRUE) %>%
-	median_qi(.width = c(.5, .9)) %>%
-	ggplot(., aes(
-		x = age_group, y = .prediction/10, 
-		color = race, fill = race, group = race,
-		ymin = .lower/10, ymax = .upper/10
-		)
-	) +
-	geom_ribbon(color = 'grey100', aes(group = .width, alpha = .width)) +
-	geom_line(size = 1) +
-	facet_grid(~race) +
-    c_pal +
-    f_pal + 
-    scale_x_discrete(
-    	breaks = c('0', '25-29', '50-54', '75-79'),
-    	labels = c(0, 25, 50, 75)
-    ) +
-    theme(legend.position = 'none') +
-    scale_alpha(range = c(.75, .25), breaks = c(.5, .9)) +
-    ylab('predicted police-related mortality rate, per 100,000') +
-    xlab('age')
-
-# ... women
-demo_scen %>%
-	mutate(pop = 100000*1000) %>%
-	filter(sex == 'Female') %>%
-	add_predicted_draws(fit, allow_new_levels = TRUE) %>%
-	median_qi(.width = c(.5, .9)) %>%
 	ggplot(., aes(
 		x = age_group, y = .prediction/1000, 
 		color = race, fill = race, group = race,
@@ -106,4 +83,43 @@ demo_scen %>%
     	labels = c(0, 25, 50, 75)
     ) +
     theme(legend.position = 'none') +
-    scale_alpha(range = c(.75, .25), breaks = c(.5, .9)) 
+    scale_alpha(range = c(.75, .25), breaks = c(.5, .9)) +
+    ylab('predicted police-related mortality rate, per 100,000') +
+    xlab('age')
+
+# ... women 
+post_pred %>%
+	filter(sex == 'Female') %>%
+	ggplot(., aes(
+		x = age_group, y = .prediction/1000, 
+		color = race, fill = race, group = race,
+		ymin = .lower/1000, ymax = .upper/1000
+		)
+	) +
+	geom_ribbon(color = 'grey100', aes(group = .width, alpha = .width)) +
+	geom_line(size = 1) +
+	facet_grid(~race) +
+    c_pal +
+    f_pal + 
+    scale_x_discrete(
+    	breaks = c('0', '25-29', '50-54', '75-79'),
+    	labels = c(0, 25, 50, 75)
+    ) +
+    theme(legend.position = 'none') +
+    scale_alpha(range = c(.75, .25), breaks = c(.5, .9)) +
+    ylab('predicted police-related mortality rate, per 100,000') +
+    xlab('age')
+
+# ... exports for team
+#post_fit_90 = demo_scen %>%
+#	mutate(pop = 100000) %>%
+#	add_fitted_draws(fit, allow_new_levels = TRUE) %>%
+#	median_qi(.width = .9)#
+
+#post_pred_90 = demo_scen %>%
+#	mutate(pop = 100000*1000) %>%
+#	add_predicted_draws(fit, allow_new_levels = TRUE) %>%
+#	median_qi(.width = .9)#
+
+#write_rds(post_fit_90,  'post_fit_90.rds')
+#write_rds(post_pred_90, 'post_pred_90.rds')
