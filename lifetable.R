5099c711f428cfb96dd057f4fddccc2dbed313de
Frank Edwards <frankalready@gmail.com>
2019-03-27 20:58:14 -0400

    add lifetable scripts

---
+++ lifetable.R
@@ -0,0 +1,53 @@
+### make period lifetables 
+## uses methods from Preston, Heuveline, and Guillot (2001)
+## and German Rodriguez (code) http://data.princeton.edu/eco572/periodlt.html
+## uses separation factors from Keyfitz and Flieger (via Rodriguez)
+
+make_life_table<-function(nat_dat, deaths){
+  nat_dat<-nat_dat%>%
+    ungroup(nat_dat)
+  ### create proportions of pop with outcome by age/year
+  nat_dat<-nat_dat%>%
+    mutate(m = deaths / pop)
+  
+  ### convert to probability 
+  ### age_period (n) != 1 for all cases
+  ### assume mid-point average time of death for within-period deaths
+  ### n = 1 if age == "0", 
+  ### n = 10 (arbitrary, assumes death by 95) if age == "85+",
+  ### n = 4 if age == "1-4"
+  ### else n = 5 for all other age groups
+  nat_dat<-nat_dat%>%
+    mutate(n = case_when(
+      age==0 ~ 1,
+      age==1 ~ 4,
+      age==85 ~ 10,
+      ! age %in% c(1, 4, 85) ~ 5
+    ))
+  
+  nat_dat<-nat_dat%>%
+    mutate(q = ### set to probability = 1 for 85+, nQx otherwise
+             ifelse(age<85,
+               n * m / (1 + (n - nax) * m),
+               1),
+           p = 1 - q)
+  ### make cumulative survival
+  nat_dat<-nat_dat%>%
+    mutate(lx = 1e5 * cumprod(c(1, p[-nrow(nat_dat)])))
+  
+  ### deaths
+  nat_dat<-nat_dat%>%
+    mutate(d = c(-diff(lx),lx[nrow(nat_dat)]))
+  ## person-years in each group
+  nat_dat<-nat_dat%>%
+    mutate(L = (lx - d) * n + d * nax,
+           t = sum(L)- cumsum(L) + L)
+  ## life expectancy
+  nat_dat<-nat_dat%>%
+    mutate(e = t/lx)
+  ### cum prevalence
+  nat_dat<-nat_dat%>%
+    mutate(c = 1-lx/1e5)
+  return(nat_dat)
+}
+
---
+++ lifetable_multiple.R
@@ -0,0 +1,68 @@
+### make multiple decrement period lifetables 
+## uses methods from Preston, Heuveline, and Guillot (2001)
+## and German Rodriguez (code) http://data.princeton.edu/eco572/periodlt.html
+## uses separation factors from Keyfitz and Flieger (via Rodriguez)
+## requires cause of death data, deaths column, and period life table (total mort)
+## Use radix 1e8 to match sim draws
+edit.na <- function(x, value) { x[is.na(x)] <- value; x}
+
+
+#### FOR DEBUGGING, use observed (imputed) and sims to check results
+# nat_dat<-fe_imp%>%
+#   filter(.imp==1)%>%
+#   filter(year ==2017)%>%
+#   filter(race=="African American", sex=="Male")%>%
+#   rename(deaths = officer_force)
+# 
+# nat_dat<-fe_postpred%>%
+#   filter(race=="African American", sex=="Male")%>%
+#   rename(deaths = median)
+# 
+# life_table<-nvss_lifetable%>%
+#   filter(race=="African American", sex=="Male")
+#######
+
+make_life_table_multiple<-function(nat_dat, deaths, life_table){
+  nat_dat<-nat_dat%>%
+    ungroup(nat_dat)%>%
+    left_join(life_table%>%
+                select(age, race, sex, deaths, d, lx, q, nax, m, n, e)%>%
+                rename(D = deaths,
+                       d_x = d,
+                       q_x = q,
+                       a = nax))
+  
+  nat_dat<-nat_dat%>%
+    mutate(q_xi = q_x * (deaths / D),
+           d_i = q_xi * lx)
+  
+  ### cause deleted decomposition
+  nat_dat<-nat_dat%>%
+    mutate(R = (d_x - d_i)/ d_x)
+  
+  nat_dat<-nat_dat%>%
+    mutate(pd = (1-q_x)^R,
+           ld = 1e5 * cumprod(c(1, pd[-length(pd)])))
+  
+  nat_dat<-nat_dat%>%
+    mutate(dd = edit.na(ld - lead(ld),
+                        tail(ld,1)),
+           qd = dd/ld,
+           ad = ifelse(age<10|age==80,
+                       n + R * (q_x/qd) * (a-n),
+                       ifelse(age>=10 & age<=75,
+                              ((-5/24) * lag(dd) + 2.5 * dd + (5/24) * lead(dd))/dd,
+                              a/R)))
+  
+  nat_dat<-nat_dat%>%
+    mutate(Ld = edit.na(dd * ad + (ld-dd) * n,
+                        tail(ld*ad, 1)))
+  
+  nat_dat<-nat_dat%>%
+    mutate(td = sum(Ld) - cumsum(Ld) + Ld,
+           ed = td/ld)
+  
+  return(nat_dat)
+}
+
+
